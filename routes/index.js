8d4e7c9c1c60ff2f3e66fffb04ea1d7fbf77aadb
Jordan Buschman <jordan.buschman@me.com>
2014-11-12 00:30:30 -0800

    <address>:<port>/ shows you what IP address and port to connect to

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var os      = require('os');
 
 var router = express.Router();
 
+function privateIP() {
+    var interfaces = os.networkInterfaces();
+    for (k in interfaces) {
+        for (k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal)
+                return address.address;
+        }
+    }
+}
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { ip: '127.0.0.1', port: process.env.PORT || 3000 });
+    res.render('index', { ip: privateIP(), port: process.env.PORT || 3000 });
 }).post('/', function(req, res) {
     res.json({ message: 'potato' });
 });
